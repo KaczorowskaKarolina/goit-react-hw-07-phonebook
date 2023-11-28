44b0e642af35d0805f3fee05272a585815a046b1
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-11-28 13:30:41 +0100

    Store.jsx modyfication

--- src/components/Atoms/Store.jsx
+++ src/components/Atoms/Store.jsx
@@ -1,12 +1,12 @@
 import { configureStore, createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchContacts = createAsyncThunk('contacts/fetchContacts', async () => {
-  const response = await fetch('https://655bc2e8ab37729791a98fce.mockapi.io/contacts');
+  const response = await fetch('https://655bc2e8ab37729791a98fce.mockapi.io');
   return response.json();
 });
 
 export const addContact = createAsyncThunk('contacts/addContact', async (contactData) => {
-  const response = await fetch('https://655bc2e8ab37729791a98fce.mockapi.io/contacts', {
+  const response = await fetch('https://655bc2e8ab37729791a98fce.mockapi.io', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -17,7 +17,7 @@ export const addContact = createAsyncThunk('contacts/addContact', async (contact
 });
 
 export const deleteContact = createAsyncThunk('contacts/deleteContact', async (contactId) => {
-  await fetch(`https://655bc2e8ab37729791a98fce.mockapi.io/contacts/${contactId}`, {
+  await fetch(`https://655bc2e8ab37729791a98fce.mockapi.io/${contactId}`, {
     method: 'DELETE',
   });
   return contactId;
